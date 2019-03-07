23a8f0c1f82a62507c00aac0e6dae1c2f5563762
Brandon Berhent <bbedward@gmail.com>
2019-03-07 15:28:46 -0500

    sleep

--- main.py
+++ main.py
@@ -27,10 +27,15 @@ async def monkey(request):
     await asyncio.sleep(0.01)
     # Remove duplicate <svg and </svg
     await run_command(f'sed -i "s%<svg%<g%g" /tmp/monkeyfiles/{address}_optimized.svg')
+    await asyncio.sleep(0.02)
     await run_command(f'sed -i "s%</svg%</g%g" /tmp/monkeyfiles/{address}_optimized.svg')
+    await asyncio.sleep(0.02)
     await run_command(f'sed -i -e "0,/<g/ s/<g/<svg/" /tmp/monkeyfiles/{address}_optimized.svg')
+    await asyncio.sleep(0.02)
     await run_command(f'sed -i "s/....$//" /tmp/monkeyfiles/{address}_optimized.svg')
+    await asyncio.sleep(0.02)
     await run_command(f'echo "</svg>" >> /tmp/monkeyfiles/{address}_optimized.svg')
+    await asyncio.sleep(0.02)
     return web.FileResponse(f'/tmp/monkeyfiles/{address}_optimized.svg')
 
 async def run_command(cmd):
