424559cb632b00735c3bb9f5bcaf1ed6a8befd8b
Brandon Berhent <bbedward@gmail.com>
2019-03-07 10:35:36 -0500

    Just use concatenation here

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def monkey(request):
                 await f.close()
             else:
                 return web.HTTPServerError(reason='monkey API returned error status')
-    await run_command('svgo /tmp/monkeyfiles/{0}.svg --enable=inlineStyles --config \'{ "plugins": [ { "inlineStyles": { "onlyMatchedOnce": false } }] }\''.format(address))
+    await run_command('svgo /tmp/monkeyfiles/'+address+'.svg --enable=inlineStyles --config \'{ "plugins": [ { "inlineStyles": { "onlyMatchedOnce": false } }] }\'')
     return web.FileResponse(f'/tmp/monkeyfiles/{address}.svg')
 
 async def run_command(cmd):
