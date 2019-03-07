7778c803e65baa416e7eb299bf9c4294b61fe8a3
bbedward <bbedward@gmail.com>
2019-03-07 17:39:31 -0500

    Add sleep

--- svgprocess.sh
+++ svgprocess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed -i "s%\'</style>%</style>%g" $1
 /home/monkey/.cargo/bin/svgcleaner $1 $2
+sleep 0.05
 sed -i "s%<svg%<g%g" $2
 sed -i "s%</svg%</g%g" $2
 sed -i -e "0,/<g/ s/<g/<svg/" $2
