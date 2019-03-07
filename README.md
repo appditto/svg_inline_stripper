ca56110233d622c90ffa9da2d96f1c027a03952a
Brandon Berhent <bbedward@gmail.com>
2019-03-07 10:19:34 -0500

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# svg_inline_stripper
-Server that optimizes SVGs before returning to client using SVGO
+# SVGO API
+
+Python asyncio web API that runs SVGs through the `svgo` tool. Uses aiofiles and aiohttp.
+
+Middleware for monkeygen.com
+
+Removes and minifies SVGs using [SVGO](https://github.com/svg/svgo), the main goal is to convert CSS styles to inline styles - but this API wrapper could be modified to use svgo in any way.
+
+# Requirements
+
+Recommended setup (NodeJS is required also for SVGO):
+
+```
+# virtualenv -p python3.6 venv
+# pip install -r requirements.txt
+# npm install -g svgo
+```
+
+# Deployment
+
+See the [aiohttp deployment documentation](https://docs.aiohttp.org/en/stable/deployment.html)
---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import aiofiles
+from aiohttp import ClientSession, log, web
+
+async def monkey(request):
+    try:
+        address = request.rel_url.query['address']
+    except KeyError:
+        return web.HTTPBadRequest(reason='address parameter is required')
+    async with ClientSession() as session:
+        url = f"http://bananomonkeys.herokuapp.com/image?address={address}"
+        async with session.get(url, timeout=30) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f'/tmp/monkeyfiles/{address}.svg', mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+            else:
+                return web.HTTPServerError(reason='monkey API returned error status')
+    await run_command(f'svgo /tmp/monkeyfiles/{address}.svg --enable=inlineStyles --config \'{ "plugins": [ { "inlineStyles": { "onlyMatchedOnce": false } }] }\'')
+    return web.FileResponse(f'/tmp/monkeyfiles/{address}.svg')
+
+async def run_command(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+app = web.Application()
+app.router.add_get('/', monkey)
+#web.run_app(app, port=9099)
